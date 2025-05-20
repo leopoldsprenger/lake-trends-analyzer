0a593e0c3632c1194af4c29b57508edf167b85f1
Leopold Sprenger <186564656+leopoldsprenger@users.noreply.github.com>
2025-05-20 19:39:52 +0200

    Implement basic CLI input method using argparse

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Lake Trend Analyzer')
+
+    parser.add_argument('input', type=str, help='Input CSV file')
+    parser.add_argument('--variables', type=str, nargs='+', help='Variables to analyze')
+
+    return parser.parse_args()
+
 def main():
-    print("Repository initialized correctly...")
+    arguments = parse_arguments()
+    filepath = arguments.input
+    variables = arguments.variables
+
+    print(f"Input file: {filepath}")
+    print(f"Output variables: {variables}")
 
 if __name__ == '__main__':
     main()
